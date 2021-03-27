e16ede29140ae68583fdeb1e0cda0e0e1ca4f276
Ilya Stepanov <39239382+KarlWild@users.noreply.github.com>
2021-03-27 12:50:46 +0300

    Update Main.java

--- com/company/Main.java
+++ com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // write your code here
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
